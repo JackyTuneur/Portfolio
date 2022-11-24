3a77d1c207ff5c3c6ece6effa6ed9945c605f94f
JackyTuneur <118255407+JackyTuneur@users.noreply.github.com>
2022-11-24 10:32:53 +0100

    up

--- script/script.js
+++ script/script.js
@@ -7,17 +7,31 @@ let goRightButtonFromCaroussel0 = document.getElementById("goRightB1");
 let goLeftButtonFromCaroussel0 = document.getElementById("goLeftB1");
 let goRightButtonFromCaroussel2 = document.getElementById("goRightB2");
 let goLeftButtonFromCaroussel2 = document.getElementById("goLeftB2");
+let loadingSlide = document.getElementById("loadingSlide");
+let actualCaroussel=1;
 
 const goToCaroussel2 = () =>{
     carousselDom.style.transform = "translateX(-100%)"
+    loadingSlide.children[2].classList.add("active");
+    loadingSlide.children[1].classList.remove("active");
+    loadingSlide.children[0].classList.remove("active");
+    actualCaroussel=2;
 }
 
 const goToCaroussel1 = () =>{
     carousselDom.style.transform = "translateX(0%)"
+    loadingSlide.children[1].classList.add("active");
+    loadingSlide.children[2].classList.remove("active");
+    loadingSlide.children[0].classList.remove("active");
+    actualCaroussel=1;
 }
 
 const goToCaroussel0 = () =>{
     carousselDom.style.transform = "translateX(100%)"
+    loadingSlide.children[0].classList.add("active");
+    loadingSlide.children[1].classList.remove("active");
+    loadingSlide.children[2].classList.remove("active");
+    actualCaroussel=0;
 }
 
 goRightButtonFromCaroussel1.addEventListener("click", () => {
@@ -43,3 +57,18 @@ goRightButtonFromCaroussel2.addEventListener("click", () => {
 goLeftButtonFromCaroussel2.addEventListener("click", () => {
     goToCaroussel1();
 })
+
+const autoSlide = () => {
+    setTimeout(() => {
+        if(actualCaroussel ===0){
+            goToCaroussel1();
+        }else if(actualCaroussel ===1){
+            goToCaroussel2();
+        }else if(actualCaroussel ===2){
+            goToCaroussel0();
+        }
+        autoSlide();
+    }, 5000);
+}
+
+autoSlide();
